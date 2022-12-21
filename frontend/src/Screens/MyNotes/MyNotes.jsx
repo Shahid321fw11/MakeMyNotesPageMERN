c1d0dfad53c3c0f58ef8a81773544d88db2b2878
Shahid321fw11 <shahid_fw11_321@masai.school>
2022-12-21 19:45:00 +0530

    My notes Page done

--- frontend/src/Screens/MyNotes/MyNotes.jsx
+++ frontend/src/Screens/MyNotes/MyNotes.jsx
@@ -1,8 +1,13 @@
-import { Button } from "react-bootstrap";
+import { Badge, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import MainScreen from "../../Components/MainScreen";
+import notes from "../../notes";
 
 const MyNotes = () => {
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you Sure?")) {
+    }
+  };
   return (
     <>
       <MainScreen title="Welcome Back Shahid Ansari">
@@ -11,6 +16,46 @@ const MyNotes = () => {
             Create New Note
           </Button>
         </Link>
+        {notes.map((note) => (
+          <Card>
+            <Card.Header style={{ display: "flex" }}>
+              <span
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                  flex: 1,
+                  cursor: "pointer",
+                  alignSelf: "center",
+                  fontSize: 18,
+                }}
+              >
+                {note.title}
+              </span>
+              <div>
+                <Button href={`/note/${note._id}`}>Edit</Button>
+                <Button
+                  variant="danger"
+                  className="mx-2"
+                  onClick={() => deleteHandler(note._id)}
+                >
+                  Delete
+                </Button>
+              </div>
+            </Card.Header>
+            <Card.Body>
+              <h4>
+                <Badge variant="success">Category - {note.category}</Badge>
+              </h4>
+              <blockquote className="blockquote mb-0">
+                <p>{note.content}</p>
+                <footer className="blockquote-footer">
+                  {" "}
+                  created on '' date
+                </footer>
+              </blockquote>
+            </Card.Body>
+          </Card>
+        ))}
       </MainScreen>
     </>
   );
